862720384a324b2f632e94441f06bbc97d51e4c6
nicorojasf <nico.rojasf@duocuc.cl>
2026-05-18 20:34:54 -0400

    correjir

--- src/main/java/com/Inventario/Inventario/repository/PrendasRepository.java
+++ src/main/java/com/Inventario/Inventario/repository/PrendasRepository.java
@@ -1,6 +1,6 @@
 package com.Inventario.Inventario.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +8,7 @@ import com.Inventario.Inventario.model.Prendas;
 
 public interface PrendasRepository extends JpaRepository<Prendas, Integer> {
 
-    List<Prendas> findById(Long id);
+    Optional<Prendas> findById(Integer id);
 
 
 
